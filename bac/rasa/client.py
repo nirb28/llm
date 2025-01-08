f1429523f4e8bebdbf0cb31797dfb26cfc79c83f
Nirbaan Maken <nirbaanm@gmail.com>
2025-01-08 14:34:07 -0500

    Enhance message routing by updating regex patterns and adding configuration loading

--- bac/rasa/client.py
+++ bac/rasa/client.py
@@ -1,4 +1,4 @@
-import requests
+import requests, os, json
 
 def send_message(message):
     url = 'http://127.0.0.1:5000/route_message'
@@ -10,7 +10,13 @@ def send_message(message):
 
 if __name__ == '__main__':
     # Example messages
-    send_message("This is a test message containing malts.")
+    #send_message("This is a test message containing MALT.")
     # send_message("This message mentions MSaaS.")
     # send_message("Here we talk about DQ4D.")
     # send_message("No keywords here.")
+    print("Current working directory:", os.getcwd())
+    # Load regex rules from config file
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+        rules = config['rules']        
+        print(rules)
